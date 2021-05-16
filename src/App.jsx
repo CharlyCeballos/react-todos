2c75a6032172675a758eab67e6ae0514fcd44041
CharlyCeballos <74975887+CharlyCeballos@users.noreply.github.com>
2021-05-16 16:35:19 -0500

    main commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React, { Fragment, useState, useRef } from "react";
+import {v4 as uuidv4} from "uuid";
+import  { TodoList } from "./components/TodoList";
+
+export function App() {
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            task: "Tarea 1",
+            completed: false,
+        },
+    ]);
+    
+    const todoTaskRef = useRef();
+
+    const handleTodoAdd= () => {
+        const task = todoTaskRef.current.value;
+        if (task === ''){
+            return;
+        };
+
+        setTodos((prevTodos) =>{
+            return[...prevTodos, {id: uuidv4(), task, completed:false}]
+        });
+
+        todoTaskRef.current.value = null;
+    };
+
+    const toggleTodo= (id) => {
+        const newTodos = [...todos];
+        const todo = newTodos.find((todo) => todo.id ===id );
+        todo.completed = !todo.completed;
+        setTodos(newTodos);
+    };
+
+    const handleClearAll = () =>{
+        const newTodo = todos.filter((todo) => !todo.completed);
+        setTodos(newTodo);
+    };
+
+    return (
+        <Fragment>
+            <TodoList todos={todos} toggleTodo={toggleTodo}/>
+            <input ref={todoTaskRef} type="text" placeholder="Nueva Tarea" />
+            <button onClick={handleTodoAdd}>🎶</button>
+            <button onClick={handleClearAll}>☠</button>
+            <div>
+                Te quedan { todos.filter((todo) => !todo.completed).length } tareas por terminar.
+            </div>
+        </Fragment>
+    );
+}
